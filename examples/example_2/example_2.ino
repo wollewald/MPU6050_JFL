cc95e6a8f1d2a2e64a8a9a01eb6fcf8ec6887de4
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-09-24 15:00:16 +0200

    Add files via upload

--- examples/example_2/example_2.ino
+++ examples/example_2/example_2.ino
@@ -1,1 +1,30 @@
+#include <MPU6050_JFL.h>
+#define I2C_ADDRESS 0x68
 
+MPU6050_JFL myMPU = MPU6050_JFL(I2C_ADDRESS);
+
+void setup() {
+  Serial.begin(9600);
+  if(myMPU.init()){
+    Serial.println("MPU6050 connected");
+  }
+  else{
+    Serial.println("MPU6050 not connected");
+    while(1); 
+  }
+  myMPU.setGyroRange(MPU6050_GYRO_RANGE_500DPS);
+}
+
+void loop(){
+  myMPU.update();
+  xyzFloat gyr = myMPU.getGyroscopeDataFromAllRawData();
+ 
+  Serial.print("gyr_x: ");
+  Serial.print(gyr.x); Serial.print('\t');
+  Serial.print("gyr_y: ");
+  Serial.print(gyr.y); Serial.print('\t');
+  Serial.print("gyr_z: ");
+  Serial.println(gyr.z);
+
+  delay(2000); 
+} 
