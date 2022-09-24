ecc471083b0b641b5d29529c8341ebf2fb75e4c4
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-09-24 14:56:05 +0200

    Add files via upload

---
+++ MPU6050_JFL.cpp
@@ -0,0 +1,138 @@
+#include <MPU6050_JFL.h>
+
+MPU6050_JFL::MPU6050_JFL(uint8_t addr){
+    i2cAddress = addr;
+}   
+
+bool MPU6050_JFL::init(){
+    //Wire.setWireTimeout();
+    accelRangeFactor = 1.0;
+    gyroRangeFactor = 1.0; 
+    bool connected = !reset();
+    delay(100); // MPU6050 needs 100 ms to reset
+    writeRegister(MPU6050_PWR_MGMT_1, 0x00); // disable sleep
+    delay(100); // give the device some time to wake up!
+    return connected;
+}
+
+bool MPU6050_JFL::reset(){
+    return writeRegister(MPU6050_PWR_MGMT_1, MPU6050_DEVICE_RESET);
+}
+
+uint8_t MPU6050_JFL::whoAmI(){
+    return readRegister(MPU6050_WHO_AM_I_MPU6050);
+}
+
+void MPU6050_JFL::setAccelRange(accel_range range){
+    uint8_t regVal = readRegister(MPU6050_ACCEL_CONFIG);
+    switch(range){
+        case MPU6050_ACCEL_RANGE_2G:  accelRangeFactor = 2.0;  break;
+        case MPU6050_ACCEL_RANGE_4G:  accelRangeFactor = 4.0;  break;
+        case MPU6050_ACCEL_RANGE_8G:  accelRangeFactor = 8.0;  break;
+        case MPU6050_ACCEL_RANGE_16G: accelRangeFactor = 16.0;  break;
+    }
+    regVal &= ~MPU6050_ACCEL_RANGE_MASK;
+    regVal |= range<<3;
+    writeRegister(MPU6050_ACCEL_CONFIG, regVal);
+}
+
+void MPU6050_JFL::setGyroRange(gyro_range range){
+    uint8_t regVal = readRegister(MPU6050_ACCEL_CONFIG);
+    switch(range){
+        case MPU6050_GYRO_RANGE_250DPS:  gyroRangeFactor = 250.0;  break;
+        case MPU6050_GYRO_RANGE_500DPS:  gyroRangeFactor = 500.0;  break;
+        case MPU6050_GYRO_RANGE_1000DPS:  gyroRangeFactor = 1000.0;  break;
+        case MPU6050_GYRO_RANGE_2000DPS: gyroRangeFactor = 2000.0;  break;  
+    }
+    regVal &= ~MPU6050_GYRO_RANGE_MASK;
+    regVal |= range;
+    writeRegister(MPU6050_GYRO_CONFIG, regVal);
+}
+
+uint8_t MPU6050_JFL::getAccelRange(){
+    uint8_t regVal = readRegister(MPU6050_ACCEL_CONFIG);
+    regVal = regVal >> 3;
+    return regVal;
+}
+
+float MPU6050_JFL::getOnlyTemperature(){
+    uint16_t rawTemp = read2Registers(MPU6050_TEMP_OUT_H);
+    float temp = ((int16_t)rawTemp)/340.0 + 36.53; // see RegMap page 30;
+    return temp;
+}
+
+xyzFloat MPU6050_JFL::getAccelerationData(){
+    uint8_t rawData[6]; 
+    xyzFloat accel;
+    readMultipleRegisters(MPU6050_ACCEL_XOUT_H, 6, rawData);
+    accel.x = ((int16_t)((rawData[0] << 8) | rawData[1]))/32768.0 * accelRangeFactor;
+    accel.y = ((int16_t)((rawData[2] << 8) | rawData[3]))/32768.0 * accelRangeFactor;
+    accel.z = ((int16_t)((rawData[4] << 8) | rawData[5]))/32768.0 * accelRangeFactor;
+    return accel;
+}   
+
+void MPU6050_JFL::getGyroscopeData(xyzFloat *gyro){
+    uint8_t rawData[6]; 
+    readMultipleRegisters(MPU6050_GYRO_XOUT_H, 6, rawData);
+    gyro->x = ((int16_t)((rawData[0] << 8) | rawData[1]))/32768.0 * gyroRangeFactor;
+    gyro->y = ((int16_t)((rawData[2] << 8) | rawData[3]))/32768.0 * gyroRangeFactor;
+    gyro->z = ((int16_t)((rawData[4] << 8) | rawData[5]))/32768.0 * gyroRangeFactor;
+}   
+
+uint8_t MPU6050_JFL::writeRegister(uint8_t reg, uint8_t regValue){
+    Wire.beginTransmission(i2cAddress);
+    Wire.write(reg);
+    Wire.write(regValue);
+
+    return Wire.endTransmission();
+}
+
+void MPU6050_JFL::update(){
+    readMultipleRegisters(MPU6050_ACCEL_XOUT_H, 14, allRawData);
+}
+
+xyzFloat MPU6050_JFL::getGyroscopeDataFromAllRawData(){
+    xyzFloat gyro;
+    readMultipleRegisters(MPU6050_GYRO_XOUT_H, 6, allRawData);
+    gyro.x = ((int16_t)((allRawData[8] << 8) | allRawData[9]))/32768.0 * gyroRangeFactor;
+    gyro.y = ((int16_t)((allRawData[10] << 8) | allRawData[11]))/32768.0 * gyroRangeFactor;
+    gyro.z = ((int16_t)((allRawData[12] << 8) | allRawData[13]))/32768.0 * gyroRangeFactor;
+    return gyro;
+}   
+
+
+uint8_t MPU6050_JFL::readRegister(uint8_t reg){
+    uint8_t data; 
+    Wire.beginTransmission(i2cAddress);         
+    Wire.write(reg);                    
+    Wire.endTransmission(false);           
+    Wire.requestFrom(i2cAddress, (uint8_t)1);  
+    data = Wire.read();                      
+    return data;                             
+}
+
+uint16_t MPU6050_JFL::read2Registers(uint8_t reg){
+    uint8_t MSB = 0, LSB = 0; // (Most / Least Significant Byte)
+    uint16_t regValue = 0;
+    Wire.beginTransmission(i2cAddress);         
+    Wire.write(reg);                    
+    Wire.endTransmission(false);           
+    Wire.requestFrom(i2cAddress, (uint8_t)2);  
+    MSB = Wire.read(); 
+    LSB = Wire.read();
+    
+    regValue = (MSB<<8) + LSB;
+    return regValue;                             
+}
+
+void MPU6050_JFL::readMultipleRegisters(uint8_t reg, uint8_t count, uint8_t *buf){
+    Wire.beginTransmission(i2cAddress);   
+    Wire.write(reg);            
+    Wire.endTransmission(false);       
+    uint8_t i = 0;
+    Wire.requestFrom(i2cAddress, count); 
+    while (Wire.available()) {
+        buf[i] = Wire.read();
+        i++;
+    }       
+}
---
+++ MPU6050_JFL.h
@@ -0,0 +1,76 @@
+#include "Arduino.h"
+#include "Wire.h"
+
+#ifndef MPU6050_JFL_H_
+#define MPU6050_JFL_H_
+
+enum accel_range{
+  MPU6050_ACCEL_RANGE_2G = 0,
+  MPU6050_ACCEL_RANGE_4G,
+  MPU6050_ACCEL_RANGE_8G,
+  MPU6050_ACCEL_RANGE_16G
+};
+
+enum gyro_range{
+  MPU6050_GYRO_RANGE_250DPS = 0x00,
+  MPU6050_GYRO_RANGE_500DPS = 0x08,
+  MPU6050_GYRO_RANGE_1000DPS = 0x10,
+  MPU6050_GYRO_RANGE_2000DPS = 0x18
+};
+
+#define MPU6050_GYRO_CONFIG      0x1B
+#define MPU6050_ACCEL_CONFIG     0x1C
+#define MPU6050_ACCEL_XOUT_H     0x3B
+#define MPU6050_ACCEL_XOUT_L     0x3C
+#define MPU6050_ACCEL_YOUT_H     0x3D
+#define MPU6050_ACCEL_YOUT_L     0x3E
+#define MPU6050_ACCEL_ZOUT_H     0x3F
+#define MPU6050_ACCEL_ZOUT_L     0x40
+#define MPU6050_TEMP_OUT_H       0x41
+#define MPU6050_TEMP_OUT_L       0x42
+#define MPU6050_GYRO_XOUT_H      0x43
+#define MPU6050_GYRO_XOUT_L      0x44
+#define MPU6050_GYRO_YOUT_H      0x45
+#define MPU6050_GYRO_YOUT_L      0x46
+#define MPU6050_GYRO_ZOUT_H      0x47
+#define MPU6050_GYRO_ZOUT_L      0x48
+#define MPU6050_PWR_MGMT_1       0x6B // Device defaults to the SLEEP mode
+#define MPU6050_WHO_AM_I_MPU6050 0x75 // Should return 0x68
+#define MPU6050_ACCEL_RANGE_MASK 0x18 // = 0b00011000
+#define MPU6050_GYRO_RANGE_MASK  0x18 // = 0b00011000
+#define MPU6050_DEVICE_RESET     0x80
+
+struct xyzFloat {
+    float x;
+    float y;
+    float z;
+};
+
+class MPU6050_JFL
+{
+public:
+    MPU6050_JFL(uint8_t addr);
+    bool init();
+    bool reset();
+    uint8_t whoAmI();
+    void setAccelRange(accel_range range);
+    uint8_t getAccelRange();
+    void setGyroRange(gyro_range range);
+    float getOnlyTemperature();
+    xyzFloat getAccelerationData();
+    void getGyroscopeData(xyzFloat *gyro);
+    void update();
+    xyzFloat getGyroscopeDataFromAllRawData();
+        
+private:
+    uint8_t i2cAddress;
+    float accelRangeFactor;
+    float gyroRangeFactor;
+    uint8_t allRawData[14];
+    uint8_t writeRegister(uint8_t reg, uint8_t regValue);
+    uint8_t readRegister(uint8_t reg);
+    uint16_t read2Registers(uint8_t reg);
+    void readMultipleRegisters(uint8_t reg, uint8_t count, uint8_t *buf); 
+};
+
+#endif
