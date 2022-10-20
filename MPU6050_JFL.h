a15c42acfe3b7af5cd55e09c43ecf384e35b8333
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-10-20 18:51:12 +0200

    Update MPU6050_JFL.h

--- MPU6050_JFL.h
+++ MPU6050_JFL.h
@@ -35,7 +35,7 @@ constexpr uint8_t MPU6050_GYRO_YOUT_L       {0x46};
 constexpr uint8_t MPU6050_GYRO_ZOUT_H       {0x47};
 constexpr uint8_t MPU6050_GYRO_ZOUT_L       {0x48};
 constexpr uint8_t MPU6050_PWR_MGMT_1        {0x6B}; // Device defaults to the SLEEP mode
-constexpr uint8_t MPU6050_WHO_AM_I_MPU6050  {0x75}; // Should return 0x68
+constexpr uint8_t MPU6050_WHO_AM_I          {0x75}; // Should return 0x68
 constexpr uint8_t MPU6050_ACCEL_RANGE_MASK  {0x18}; // = 0b00011000
 constexpr uint8_t MPU6050_GYRO_RANGE_MASK   {0x18}; // = 0b00011000
 constexpr uint8_t MPU6050_DEVICE_RESET      {0x80};
