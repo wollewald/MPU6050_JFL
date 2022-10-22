92fade7ca8009d4be989942ed8b701f7d8c259aa
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-10-22 12:42:15 +0200

    Add files via upload

--- MPU6050_JFL.cpp
+++ MPU6050_JFL.cpp
@@ -1,5 +1,30 @@
 #include <MPU6050_JFL.h>
 
+namespace{
+    constexpr uint8_t MPU6050_GYRO_CONFIG       {0x1B};
+    constexpr uint8_t MPU6050_ACCEL_CONFIG      {0x1C};
+    constexpr uint8_t MPU6050_ACCEL_XOUT_H      {0x3B};
+    constexpr uint8_t MPU6050_ACCEL_XOUT_L      {0x3C};
+    constexpr uint8_t MPU6050_ACCEL_YOUT_H      {0x3D};
+    constexpr uint8_t MPU6050_ACCEL_YOUT_L      {0x3E};
+    constexpr uint8_t MPU6050_ACCEL_ZOUT_H      {0x3F};
+    constexpr uint8_t MPU6050_ACCEL_ZOUT_L      {0x40};
+    constexpr uint8_t MPU6050_TEMP_OUT_H        {0x41};
+    constexpr uint8_t MPU6050_TEMP_OUT_L        {0x42};
+    constexpr uint8_t MPU6050_GYRO_XOUT_H       {0x43};
+    constexpr uint8_t MPU6050_GYRO_XOUT_L       {0x44};
+    constexpr uint8_t MPU6050_GYRO_YOUT_H       {0x45};
+    constexpr uint8_t MPU6050_GYRO_YOUT_L       {0x46};
+    constexpr uint8_t MPU6050_GYRO_ZOUT_H       {0x47};
+    constexpr uint8_t MPU6050_GYRO_ZOUT_L       {0x48};
+    constexpr uint8_t MPU6050_PWR_MGMT_1        {0x6B}; // Device defaults to the SLEEP mode
+    constexpr uint8_t MPU6050_WHO_AM_I          {0x75}; // Should return 0x68
+    constexpr uint8_t MPU6050_ACCEL_RANGE_MASK  {0x18}; // = 0b00011000
+    constexpr uint8_t MPU6050_GYRO_RANGE_MASK   {0x18}; // = 0b00011000
+    constexpr uint8_t MPU6050_DEVICE_RESET      {0x80};
+    constexpr uint8_t MPU6050_DEVICE_SLEEP      {0x40};
+}
+
 MPU6050_JFL::MPU6050_JFL(const uint8_t addr)
     : i2cAddress{addr}
 {
@@ -52,10 +77,10 @@ void MPU6050_JFL::setAccelRange(accel_range range){
 void MPU6050_JFL::setGyroRange(GyroRange range){
     uint8_t regVal = readRegister(MPU6050_ACCEL_CONFIG);
     switch(range){
-        case GyroRange::DPS250:  gyroRangeFactor = 250.0;  break;
-        case GyroRange::DPS500:  gyroRangeFactor = 500.0;  break;
-        case GyroRange::DPS1000: gyroRangeFactor = 1000.0;  break;
-        case GyroRange::DPS2000: gyroRangeFactor = 2000.0;  break;  
+        case GyroRange::MPU6050_250DPS:  gyroRangeFactor = 250.0;  break;
+        case GyroRange::MPU6050_500DPS:  gyroRangeFactor = 500.0;  break;
+        case GyroRange::MPU6050_1000DPS: gyroRangeFactor = 1000.0;  break;
+        case GyroRange::MPU6050_2000DPS: gyroRangeFactor = 2000.0;  break;  
     }
     regVal &= ~MPU6050_GYRO_RANGE_MASK;
     regVal |= static_cast<uint8_t>(range);
--- MPU6050_JFL.h
+++ MPU6050_JFL.h
@@ -12,34 +12,11 @@ enum accel_range{
 };
 
 enum class GyroRange : uint8_t{
-    DPS250  = 0x00,
-    DPS500  = 0x08,
-    DPS1000 = 0x10,
-    DPS2000 = 0x18
+    MPU6050_250DPS  = 0x00,
+    MPU6050_500DPS  = 0x08,
+    MPU6050_1000DPS = 0x10,
+    MPU6050_2000DPS = 0x18
 };
-
-constexpr uint8_t MPU6050_GYRO_CONFIG       {0x1B};
-constexpr uint8_t MPU6050_ACCEL_CONFIG      {0x1C};
-constexpr uint8_t MPU6050_ACCEL_XOUT_H      {0x3B};
-constexpr uint8_t MPU6050_ACCEL_XOUT_L      {0x3C};
-constexpr uint8_t MPU6050_ACCEL_YOUT_H      {0x3D};
-constexpr uint8_t MPU6050_ACCEL_YOUT_L      {0x3E};
-constexpr uint8_t MPU6050_ACCEL_ZOUT_H      {0x3F};
-constexpr uint8_t MPU6050_ACCEL_ZOUT_L      {0x40};
-constexpr uint8_t MPU6050_TEMP_OUT_H        {0x41};
-constexpr uint8_t MPU6050_TEMP_OUT_L        {0x42};
-constexpr uint8_t MPU6050_GYRO_XOUT_H       {0x43};
-constexpr uint8_t MPU6050_GYRO_XOUT_L       {0x44};
-constexpr uint8_t MPU6050_GYRO_YOUT_H       {0x45};
-constexpr uint8_t MPU6050_GYRO_YOUT_L       {0x46};
-constexpr uint8_t MPU6050_GYRO_ZOUT_H       {0x47};
-constexpr uint8_t MPU6050_GYRO_ZOUT_L       {0x48};
-constexpr uint8_t MPU6050_PWR_MGMT_1        {0x6B}; // Device defaults to the SLEEP mode
-constexpr uint8_t MPU6050_WHO_AM_I          {0x75}; // Should return 0x68
-constexpr uint8_t MPU6050_ACCEL_RANGE_MASK  {0x18}; // = 0b00011000
-constexpr uint8_t MPU6050_GYRO_RANGE_MASK   {0x18}; // = 0b00011000
-constexpr uint8_t MPU6050_DEVICE_RESET      {0x80};
-constexpr uint8_t MPU6050_DEVICE_SLEEP      {0x40};
 
 struct xyzFloat {
     float x;
